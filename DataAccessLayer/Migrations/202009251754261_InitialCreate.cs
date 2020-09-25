c5edd1e135b0ee0def22047140f028df2cbf15da
Lars Nysom <LANY@ucn.dk>
2020-09-25 20:03:00 +0200

    Changed migration

---
+++ DataAccessLayer/Migrations/202009251754261_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿namespace DataAccessLayerEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cars",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Brand = c.String(),
+                        Fuel = c.Int(nullable: false),
+                        PassengerCapacity = c.Int(nullable: false),
+                        KilometersDriven = c.Int(nullable: false),
+                        Location_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Locations", t => t.Location_Id)
+                .Index(t => t.Location_Id);
+            
+            CreateTable(
+                "dbo.Locations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Address = c.String(),
+                        Zip = c.String(),
+                        City = c.String(),
+                        Phone = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Cars", "Location_Id", "dbo.Locations");
+            DropIndex("dbo.Cars", new[] { "Location_Id" });
+            DropTable("dbo.Locations");
+            DropTable("dbo.Cars");
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace DataAccessLayerEF.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAccessLayer.CarContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "DataAccessLayer.CarContext";
+        }
+
+        protected override void Seed(DataAccessLayer.CarContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
